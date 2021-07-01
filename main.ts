3641ea2f5177c34b5aceb5105527072009027893
minedgeekble <86029271+minedgeekble@users.noreply.github.com>
2021-07-01 09:21:26 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
-	
-})
+basic.showNumber(5)
+basic.pause(1000)
+basic.showNumber(4)
+basic.pause(1000)
+basic.showNumber(3)
+basic.pause(1000)
+basic.showNumber(2)
+basic.pause(1000)
+basic.showNumber(1)
+basic.pause(1000)
+basic.showString("g")
+basic.showString("o")
+basic.showString("!")
